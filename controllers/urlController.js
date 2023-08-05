3d6c4b54037be4d57d666c850f7b585335bbaf57
yy933 <emilyyu025@gmail.com>
2023-08-05 18:39:13 +0800

    refactor: refactor major functions into controllers

---
+++ controllers/urlController.js
@@ -0,0 +1,66 @@
+const shortenUrl = require('../models/shortenUrls')
+const idToShortUrl = require('../helpers/generateShortUrl')
+const validUrl = require('is-url-http')
+const baseUrl = process.env.BASE_URL
+
+const urlController = {
+  convertUrl: (req, res, next) => {
+    const { originalUrl } = req.body
+    const isValidUrl = validUrl(originalUrl)
+    let newUrl
+    if (!isValidUrl) {
+      console.log('Not a valid url!')
+      res.render('index', { originalUrl, invalidUrl: true })
+      return res.status(404)
+    }
+    shortenUrl
+      .find()
+      .lean()
+      .then(data => {
+      // check if the input url already exists in database
+        newUrl = data.find(item => item.originalUrl === originalUrl)
+        if (newUrl) {
+          newUrl = baseUrl + newUrl.shortUrl
+          return
+        }
+        // if short string already existed, regenerate a random short string
+        let shortString = idToShortUrl()
+        if (data.some(item => item.shortUrl === shortString)) {
+          shortString = idToShortUrl()
+        }
+        newUrl = baseUrl + shortString
+        // create new data
+        shortenUrl.create({
+          originalUrl,
+          shortUrl: shortString
+        })
+      })
+      .then(() => res.render('show', { newUrl, originalUrl }))
+      .catch((error) => {
+        console.log(error)
+        return res.render('error', { error_message: error.message })
+      })
+  },
+  getOrigin: (req, res) => {
+    const { shortUrl } = req.params
+    shortenUrl
+      .findOne({ shortUrl })
+      .lean()
+      .then(data => {
+        if (data) {
+          return res.redirect(data.originalUrl)
+        } else {
+          return res.render('error', {
+            error_message: `Url ${process.env.BASE_URL}${shortUrl} does not exist!`
+          })
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        return res.render('error', { error_message: error.message })
+      })
+  }
+
+}
+
+module.exports = urlController
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,75 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const shortenUrl = require('../../models/shortenUrls')
-const idToShortUrl = require('../../generateShortUrl')
-const validUrl = require('is-url-http')
+const urlController = require('../../controllers/urlController')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-router.get('/', (req, res) => {
-  return res.render('index')
-})
-
 // convert to short url and save data
-const baseUrl = process.env.BASE_URL
-router.post('/', (req, res) => {
-  const { originalUrl } = req.body
-  const isValidUrl = validUrl(originalUrl)
-  let newUrl
-  if (!isValidUrl) {
-    console.log('Not a valid url!')
-    res.render('index', { originalUrl, invalidUrl: true })
-    return res.status(404)
-  }
-  shortenUrl
-    .find()
-    .lean()
-    .then(data => {
-      // check if the input url already exists in database
-      newUrl = data.find(item => item.originalUrl === originalUrl)
-      if (newUrl) {
-        newUrl = baseUrl + newUrl.shortUrl
-        return
-      }
-      // if short string already existed, regenerate a random short string
-      let shortString = idToShortUrl()
-      if (data.some(item => item.shortUrl === shortString)) {
-        shortString = idToShortUrl()
-      }
-      newUrl = baseUrl + shortString
-      // create new data
-      shortenUrl.create({
-        originalUrl,
-        shortUrl: shortString
-      })
-    })
-    .then(() => res.render('show', { newUrl, originalUrl }))
-    .catch((error) => {
-      console.log(error)
-      return res.render('error', { error_message: error.message })
-    })
-})
+router.post('/', urlController.convertUrl)
 
 // make short url work in the browser
-router.get('/:shortUrl', (req, res) => {
-  const { shortUrl } = req.params
-  shortenUrl
-    .findOne({ shortUrl })
-    .lean()
-    .then(data => {
-      if (data) {
-        return res.redirect(data.originalUrl)
-      } else {
-        return res.render('error', {
-          error_message: `Url ${process.env.BASE_URL}${shortUrl} does not exist!`
-        })
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      return res.render('error', { error_message: error.message })
-    })
+router.get('/:shortUrl', urlController.getOrigin)
+
+router.get('/', (req, res, next) => {
+  try {
+    return res.render('index')
+  } catch (error) { next(error) }
 })
 
 module.exports = router
